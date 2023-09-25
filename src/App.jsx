42f7fc810c8c05d9aa1dfe70ee647f8bbd94bfc5
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2023-09-25 17:09:33 -0500

    Initial-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 
 import Avatar from "./atoms/avatar/avatar";
+import Card from "./atoms/card/card";
 import Footer from "./atoms/footer/footer";
 import Header from "./atoms/header/header";
 
@@ -10,8 +11,10 @@ function App() {
     <div className="App">
       <Header title="Welcome"/>
       <Footer content="Welcome to my first react project" />
-      <Avatar 
-        description="My Avatar"
+      <Avatar description="My Avatar"/>
+      <Card 
+      title="Select title"
+      content="Insert content"
       />
     </div>
   );
