f79f82af88f46052a635b5b44ed3d23a679b5bf4
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2023-09-25 12:30:23 -0500

    Initial-header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+import Header from "./atoms/header/header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Welcome"/>
+    </div>
+  );
+}
+
+export default App;
