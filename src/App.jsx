0ee77f817f91fd378118173fcbcc9641f817d0d9
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2023-09-27 20:40:39 -0500

    Quiz React 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "./App.css"
 
 import Avatar from "./atoms/avatar/avatar";
 import Badge from "./atoms/badge/badge";
+import Counter from "./atoms/card/counter";
 import Card from "./atoms/card/card";
 import Footer from "./atoms/footer/footer";
 import Header from "./atoms/header/header";
 
+
 import myAvatar from "./atoms/avatar/myAvatar.svg"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header title="React Beginner's Quiz"/>
       <Avatar imageSrc={myAvatar} description="This is My Avatar"/>
+      <Counter />
       <Card title="Select title 1" content="Insert content 1" />
       <Card title="Select title 2" content="Insert content 2" />
       <Card title="Select title 3" content="Insert content 3" />
@@ -22,4 +25,3 @@ function App() {
   );
 }
 
-export default App;
